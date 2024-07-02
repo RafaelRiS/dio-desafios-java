c1b2fb14b1879f52542de0a9de305e221ff5f084
Rafael <ralkribeiro@hotmail.com>
2024-07-02 03:45:23 -0300

    Atualizacao 02/07/2024

--- DesafioControleFluxo/src/Main.java
+++ DesafioControleFluxo/src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 // Inicia-se o programa com esta classe main
 
 public class Main {
-    /**
-     * Método principal utilizado para receber parâmetros do usuario e iniciar a contagem.
-     */
+    
+    //Método principal utilizado para receber parâmetros do usuario e iniciar a contagem.
+
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in); // entrada onde sao adquiridos os dados
 
@@ -17,8 +17,8 @@ public class Main {
 
         try {
             Contador.contar(parametroUm, parametroDois); // Espera a contagem
-        } catch (ParametrosInvalidosException e) { // Captura a excecao se ela for lancada
-            System.out.println(e.getMessage()); // Imprime a mensagem da exceção
+        } catch (ParametrosInvalidosException e) {// Captura a excecao se ela for lancada
+            System.out.println(e.getMessage());// Imprime a mensagem da exceção
         } finally {
             terminal.close(); // Termina o programa de scanner
         }
